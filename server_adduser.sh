6011d5e839ed80248ae73013b5b100d0717118ed
HYUN PARK <49754293+nemodleo@users.noreply.github.com>
2021-02-22 13:37:28 +0900

    Update server_adduser.sh

--- server_adduser.sh
+++ server_adduser.sh
@@ -16,19 +16,19 @@ name=$(echo $name_raw | cut -f 1 -d"_")" "$(echo $name_raw | cut -f 2 -d"_")
 
 
 # adduser in gateway
-adduser -c $name --conf /etc/connectome/adduser.conf --uid $uid $userid
+adduser -c "$name" --conf /etc/connectome/adduser.conf --uid $uid $userid
 
 # adduser in other nodes
 nodes="master node1 node2"
 for node in $nodes; do 
 ssh -T -A conmaster@$node << EOF
 bash
-adduser -c $name --conf /etc/connectome/adduser.conf --uid $uid $userid
+adduser -c "$name" --conf /etc/connectome/adduser.conf --uid $uid $userid
 EOF
 done
 ssh -T -A conmaster@storage << EOF
 bash
-adduser -c $name --conf /etc/connectome/adduser.conf --uid $uid $userid
+adduser -c "$name" --conf /etc/connectome/adduser.conf --uid $uid $userid
 mkdir - /data/connectome/$userid
 chown -R $userid:connectome /data/connectome/$userid
 EOF
