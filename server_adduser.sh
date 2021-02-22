bdfc4c18d49e5b48fa49d23e87212268bbcc26ae
HYUN PARK <49754293+nemodleo@users.noreply.github.com>
2021-02-22 14:04:39 +0900

    Update server_adduser.sh

--- server_adduser.sh
+++ server_adduser.sh
@@ -16,19 +16,56 @@ name=$(echo $name_raw | cut -f 1 -d"_")" "$(echo $name_raw | cut -f 2 -d"_")
 
 
 # adduser in gateway
-adduser -c "$name" --conf /etc/connectome/adduser.conf --uid $uid $userid
+adduser -c $name --conf /etc/connectome/adduser.conf --uid $uid $userid
 
 # adduser in other nodes
 nodes="master node1 node2"
 for node in $nodes; do 
 ssh -T -A conmaster@$node << EOF
 bash
-adduser -c "$name" --conf /etc/connectome/adduser.conf --uid $uid $userid
+adduser -c $name --conf /etc/connectome/adduser.conf --uid $uid $userid
+cat << DOCKERFILE > /home/connectome/$userid/docker/Dockerfile
+FROM pytorch/pytorch #
+
+RUN apt-get update && \
+    apt-get -y install sudo 
+    
+RUN pip3 install --upgrade pip3
+RUN pip3 install jupyter
+
+RUN groupadd -g 10000 connectome
+RUN useradd -m -u $uid -g 10000 $userid
+RUN passwd -d $userid
+RUN usermod -G sudo $userid
+
+EXPOSE $uid
+EXPOSE 8888
+USER $userid
+
+###
+
+# If you need other operation, write down on this part!
+
+
+
+
+
+
+
+
+
+
+
+###
+
+DOCKERFILE
+chown -R $userid:connectome /home/connectome/$userid/docker/Dockerfile
 EOF
 done
 ssh -T -A conmaster@storage << EOF
 bash
-adduser -c "$name" --conf /etc/connectome/adduser.conf --uid $uid $userid
+adduser -c $name --conf /etc/connectome/adduser.conf --uid $uid $userid
 mkdir - /data/connectome/$userid
 chown -R $userid:connectome /data/connectome/$userid
 EOF
+
