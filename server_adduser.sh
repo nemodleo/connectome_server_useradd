1a082423a48a0bad6f327a9f653baf9e15202faf
HYUN PARK <49754293+nemodleo@users.noreply.github.com>
2021-02-24 12:09:38 +0900

    Update server_adduser.sh

--- server_adduser.sh
+++ server_adduser.sh
@@ -21,42 +21,6 @@ echo $sudoPW | sudo -S adduser --conf /etc/connectome/adduser.conf --uid $uid --
 echo $sudoPW | sudo -S usermod -c "$name" $userid
 echo $sudoPW | sudo -S sudo chpasswd <<<"$userid:$userid"
 echo $sudoPW | sudo -S usermod -aG docker $userid
-cat << DOCKERFILE > /home/connectome/$userid/docker/Dockerfile
-FROM pytorch/pytorch #
-
-RUN apt-get update && \
-    apt-get -y install sudo 
-    
-RUN pip3 install --upgrade pip3
-RUN pip3 install jupyter
-
-RUN groupadd -g 10000 connectome
-RUN useradd -m -u $uid -g 10000 $userid
-RUN passwd -d $userid
-RUN usermod -G sudo $userid
-
-EXPOSE $uid
-EXPOSE 8888
-USER $userid
-
-###
-
-# If you need other operation, write down on this part!
-
-
-
-
-
-
-
-
-
-
-
-###
-
-DOCKERFILE
-echo $sudoPW | sudo -S chown -R $userid:connectome /home/connectome/$userid/docker/Dockerfile
 EOF
 done
 sshpass -p $sudoPW ssh -T -A conmaster@storage << EOF
