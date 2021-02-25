d282dcb1f5a1166238ec193f8917e4f030eae4b6
HYUN PARK <49754293+nemodleo@users.noreply.github.com>
2021-02-25 17:22:45 +0900

    Update server_adduser.sh

--- server_adduser.sh
+++ server_adduser.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
-
-uid=$1
-name=$2
-userid=ct$uid
+userid=$1
+uid=$2
+name=$3
 
 # adduser in gateway
 sudo -S adduser --conf /etc/connectome/adduser.conf --uid $uid --disabled-password --gecos "" $userid
 sudo -S usermod -c "$name" $userid
-sudo -S sudo chpasswd <<<"$userid:$userid"
+sudo -S passwd -d $userid
 sudo -S usermod -aG docker $userid
 
 # adduser in other nodes
 nodes="master node1 node2"
 for node in $nodes; do 
-  ssh -T -A conmaster@$node <<-EOF
-    bash
-    sudo -S adduser --conf /etc/connectome/adduser.conf --uid $uid --disabled-password --gecos "" $userid
-    sudo -S usermod -c "$name" $userid
-    sudo -S sudo chpasswd <<<"$userid:$userid"
-    sudo -S usermod -aG docker $userid
-  EOF
+sshpass -p $sudoPW ssh -T -A conmaster@$node << EOF
+bash
+sudo -S adduser --conf /etc/connectome/adduser.conf --uid $uid --disabled-password --gecos "" $userid
+sudo -S usermod -c "$name" $userid
+sudo -S passwd -d $userid
+sudo -S usermod -aG docker $userid
+EOF
 done
-ssh -T -A conmaster@storage <<-EOF
-  bash
-  sudo -S adduser --conf /etc/connectome/adduser.conf --uid $uid --disabled-password --gecos "" $userid
-  sudo -S usermod -c "$name" $userid
-  sudo -S sudo chpasswd <<<"$userid:$userid"
-  sudo -S usermod -aG docker $userid
-  sudo -S mkdir -p /data/connectome/$userid
-  sudo -S chown -R $userid:connectome /data/connectome/$userid
+sshpass -p $sudoPW ssh -T -A conmaster@storage << EOF
+bash
+sudo -S adduser --conf /etc/connectome/adduser.conf --uid $uid --disabled-password --gecos "" $userid
+sudo -S usermod -c "$name" $userid
+sudo -S passwd -d $userid
+sudo -S mkdir -p /data/connectome/$userid
+sudo -S chown -R $userid:connectome /data/connectome/$userid
 EOF
