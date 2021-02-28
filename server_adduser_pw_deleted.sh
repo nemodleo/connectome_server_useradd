cdee6b3520c97794f2e5a1a7aa72369fd03052b9
HYUN PARK <49754293+nemodleo@users.noreply.github.com>
2021-02-28 17:40:50 +0900

    Create server_adduser_pw_deleted.sh

---
+++ server_adduser_pw_deleted.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+userid=$1
+uid=$2
+name=$3
+
+# adduser in gateway
+sudo -S adduser --conf /etc/connectome/adduser.conf --uid $uid --disabled-password --gecos "" $userid
+sudo -S usermod -c "$name" $userid
+sudo -S sudo passwd -d $userid
+sudo -S usermod -aG docker $userid
+
+# adduser in other nodes
+nodes="master node1 node2"
+for node in $nodes; do 
+ssh -T -A conmaster@$node <<-EOF
+  bash
+  sudo -S adduser --conf /etc/connectome/adduser.conf --uid $uid --disabled-password --gecos "" $userid
+  sudo -S usermod -c "$name" $userid
+  sudo -S sudo passwd -d $userid
+  sudo -S usermod -aG docker $userid
+EOF
+done
+ssh -T -A conmaster@storage <<-EOF
+bash
+  sudo -S adduser --conf /etc/connectome/adduser.conf --uid $uid --disabled-password --gecos "" $userid
+  sudo -S usermod -c "$name" $userid
+  sudo -S sudo passwd -d $userid
+  sudo -S mkdir -p /data/connectome/$userid
+  sudo -S chown -R $userid:connectome /data/connectome/$userid
+EOF
