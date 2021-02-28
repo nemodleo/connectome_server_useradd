410fa5bed4a7fb99ac989b689c733572fd24857d
HYUN PARK <49754293+nemodleo@users.noreply.github.com>
2021-02-28 17:39:41 +0900

    Update and rename server_adduser.sh to server_adduser_pw.sh

--- server_adduser_pw.sh
+++ server_adduser_pw.sh
@@ -6,7 +6,7 @@ name=$3
 # adduser in gateway
 sudo -S adduser --conf /etc/connectome/adduser.conf --uid $uid --disabled-password --gecos "" $userid
 sudo -S usermod -c "$name" $userid
-sudo -S passwd -d $userid
+sudo -S sudo chpasswd <<<"$userid:$userid"
 sudo -S usermod -aG docker $userid
 
 # adduser in other nodes
@@ -16,7 +16,7 @@ ssh -T -A conmaster@$node <<-EOF
   bash
   sudo -S adduser --conf /etc/connectome/adduser.conf --uid $uid --disabled-password --gecos "" $userid
   sudo -S usermod -c "$name" $userid
-  sudo -S passwd -d $userid
+  sudo -S sudo chpasswd <<<"$userid:$userid"
   sudo -S usermod -aG docker $userid
 EOF
 done
@@ -24,7 +24,7 @@ ssh -T -A conmaster@storage <<-EOF
 bash
   sudo -S adduser --conf /etc/connectome/adduser.conf --uid $uid --disabled-password --gecos "" $userid
   sudo -S usermod -c "$name" $userid
-  sudo -S passwd -d $userid
+  sudo -S sudo chpasswd <<<"$userid:$userid"
   sudo -S mkdir -p /data/connectome/$userid
   sudo -S chown -R $userid:connectome /data/connectome/$userid
 EOF
